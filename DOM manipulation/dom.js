6bcdc1e7004f35615761e38ea6a018f22b6b756b
vasanth1066 <vasanthanuvavi@gmail.com>
2023-10-30 22:05:17 +0530

    getelementbyid changed background style color.

--- DOM manipulation/dom.js
+++ DOM manipulation/dom.js
@@ -7,17 +7,28 @@ let header=document.getElementById('header-title');
 //console.log(header.textContent='hello')
 
 //console.log(header.textContent='hello')
+
+//----------getElementById-------------------------
+
 let head=document.getElementById('main-header');
 head.style.borderBottom  = ' solid 4px #000';
 
 let additem=document.getElementById('titleid');
-//additem.innerHTML=<b>Add Items</b>
 
-additem.style="font-weight:bold" 
+additem.style.fontWeight="bold"; 
 additem.style.color='green'
 
+//console.log(additem);
 
+//--------------------------getElementsByClassName----------
 
-console.log(additem);
+let items=document.getElementsByClassName('list-group-item');
 
+items[2].style.backgroundColor="green"
+
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight="bold"
+}
+
+console.log(items[2]);
 
