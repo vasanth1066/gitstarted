f979c8aa3f4a490f53b97fbbe76134baf62d7418
vasanth1066 <vasanthanuvavi@gmail.com>
2023-10-30 21:34:21 +0530

    Additem text to bold and color changed to green

---
+++ DOM manipulation/dom.js
@@ -0,0 +1,23 @@
+//console.log(document);
+
+//console.log(document.title);
+
+let header=document.getElementById('header-title');
+
+//console.log(header.textContent='hello')
+
+//console.log(header.textContent='hello')
+let head=document.getElementById('main-header');
+head.style.borderBottom  = ' solid 4px #000';
+
+let additem=document.getElementById('titleid');
+//additem.innerHTML=<b>Add Items</b>
+
+additem.style="font-weight:bold" 
+additem.style.color='green'
+
+
+
+console.log(additem);
+
+
