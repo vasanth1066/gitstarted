d93e551eb8ecad207508c78dfe0315ea5038445c
vasanth1066 <vasanthanuvavi@gmail.com>
2023-10-31 10:07:47 +0530

    querySelector -querySelectorAll

--- DOM manipulation/dom.js
+++ DOM manipulation/dom.js
@@ -1,3 +1,5 @@
+// -------------------------------DOM manipulation
+
 //console.log(document);
 
 //console.log(document.title);
@@ -19,24 +21,61 @@ additem.style.color='green'
 
 //console.log(header);
 
-//--------------------------getElementsByClassName----------
+// //--------------------------getElementsByClassName----------
 
-let items=document.getElementsByClassName('list-group-item');
+// let items=document.getElementsByClassName('list-group-item');
 
-items[2].style.backgroundColor="green"
+// items[2].style.backgroundColor="green"
 
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight="bold"
-}
+// for(let i=0;i<items.length;i++){
+//     items[i].style.fontWeight="bold"
+// }
 
-console.log(items);
+// //console.log(items);
 
 //--------------------------getElementsByTagName----------
 
-let li=document.getElementsByTagName('li');
+// let li=document.getElementsByTagName('li');
 
-li[4].style.backgroundColor="yellow"
+//li[4].style.backgroundColor="yellow"
 
-console.log(li);
+//console.log(li);
+
+//-------------------------- querySelector ----------
+//  let firstitem=document.querySelector('.list-group-item')
+
+//  console.log(firstitem);
+
+// let lastItem=document.querySelector('.list-group-item:last-child');
+// lastItem.style.color='red';
+// console.log(lastItem);
+
+// let seconditem=document.querySelector
+// ('.list-group-item:nth-child(2)');
+
+// seconditem.style.backgroundColor='green'
+
+// //console.log(seconditem);
 
 
+// let thirditem=document.querySelector
+// ('.list-group-item:nth-child(3)');
+
+// thirditem.style.display='none';
+
+// //console.log(thirditem);
+
+//-------------------------- querySelectorAll ----------
+
+let secondItem=document.querySelectorAll('.list-group-item');
+secondItem[1].style.color='green';
+
+//console.log(secondItem);
+
+let odditem=document.querySelectorAll
+('li:nth-child(odd)');
+
+console.log(odditem);
+for(let i=0;i<odditem.length;i++){
+    odditem[i].style.backgroundColor='green';
+}
