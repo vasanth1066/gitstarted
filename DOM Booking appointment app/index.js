0225def11196022897134c3f7d5207cec54c9c0e
vasanth1066 <vasanthanuvavi@gmail.com>
2023-11-01 16:20:29 +0530

    letstoreuser in right format-store data in object

--- DOM Booking appointment app/index.js
+++ DOM Booking appointment app/index.js
@@ -15,17 +15,44 @@ function buttonfun(e){
     let desc=document.getElementById('descid').value;
     
 
-    localStorage.setItem('Name',name)
-    localStorage.setItem('phone',phone)
-    localStorage.setItem('desc',desc)
+    // localStorage.setItem('Name',name);
+    // localStorage.setItem('phone',phone);
+    // localStorage.setItem('desc',desc);
 
 
-    console.log('Displayed Name form Local-storgae:  '+localStorage.getItem('Name',name))
-    console.log('Displayed Phone form Local-storgae:  '+localStorage.getItem('phone',phone))
-    console.log('Displayed Desc form Local-storgae:  '+localStorage.getItem('desc',desc))
+    // console.log('Displayed Name form Local-storgae:  '+localStorage.getItem('Name'))
+    // console.log('Displayed Phone form Local-storgae:  '+localStorage.getItem('phone'))
+    // console.log('Displayed Desc form Local-storgae:  '+localStorage.getItem('desc'))
 
+
+    //-------storing data in object--------
+
+    let myobject ={
+        name: name,
+        phonenumber: phone,
+        description:desc
+
+
+    };
+    
+    // changing object to string to store in local storage
+    // because  we cannot store data as object in local storage though 
+    //we converting  it to an string
+
+    let myobject_serliaized=JSON.stringify(myobject);
+
+    localStorage.setItem ('myobject',myobject_serliaized);  // object are stored in local storaage 
+
+
+    //retreieving the object form localstorage
+    var myobject_deserliaized = JSON.parse(localStorage.getItem('myobject'));
+
+   
+    console.log(myobject_deserliaized);
+   
+    console.log(localStorage);
 
 
 }
-//console.log(btn);
+
 
